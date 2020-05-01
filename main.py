b99931d49f926eea8617ae75e95fa85e419bdad4
darth-sid <55854690+darth-sid@users.noreply.github.com>
2020-05-01 13:46:26 -0700

    e

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ Black = (0,0,0)
 Yellow = (255,225,0)
 Brown = (139,69,19)
 
+X='x'
+Y='y'
+xvec = 2
+yvec = 4 
+
 size = 320
 screen = pygame.display.set_mode((size, size))
 screen.fill(Black)
@@ -15,7 +20,7 @@ screen.fill(Black)
 running = True
 
 
-class Ball(pygame.sprite.Sprite):
+class Butter(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((size/6,size/16))
@@ -26,6 +31,11 @@ class Ball(pygame.sprite.Sprite):
     def move(self,xvec,yvec):
         self.rect.x += xvec
         self.rect.y += yvec
+    def pos(self, xy):
+        if xy == X:
+            return self.rect.x
+        elif xy == Y:
+            return self.rect.y
 
 class Bread(pygame.sprite.Sprite):
     def __init__(self):
@@ -37,11 +47,18 @@ class Bread(pygame.sprite.Sprite):
         self.rect.y = size - 50
     def move(self, vec):
         self.rect.x += vec
-        
-ball = Ball()
+    def follow(self, target):
+        self.rect.x = target[0]-40
+    def pos(self, xy):
+        if xy == X:
+            return self.rect.x
+        elif xy == Y:
+            return self.rect.y
+
+butter = Butter()
 bageht = Bread()
 
-sprites=pygame.sprite.Group((ball,bageht))
+sprites=pygame.sprite.Group((butter,bageht))
 while running:
     key = pygame.key.get_pressed()
 
@@ -50,11 +67,32 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
-    if key[K_RIGHT]:
-        bageht.move(1)
-    if key[K_LEFT]:
-        bageht.move(-1)
-    ball.move(0,1)
+#    if key[K_RIGHT]:
+#        bageht.move(1)
+#    if key[K_LEFT]:
+#        bageht.move(-1)
+
+    if butter.pos(X) >= bageht.pos(X) and butter.pos(X) <= bageht.pos(X)+40:
+        aligned = True
+        print( "hoi")
+    else:
+        aligned = False
+
+
+    #roof detection
+    if butter.pos(Y) <= 0:
+        yvec = yvec * -1
+
+	#wall detection
+    if butter.pos(X) <= 0 or butter.pos(X) >= size-40:
+        xvec = xvec * -1
+
+    butter.move(xvec,yvec)
+
+    bageht.follow(pygame.mouse.get_pos())
+   
+    if butter.pos(Y) >= 340:
+        running = False
 
     screen.fill(Black)
     sprites.draw(screen)
