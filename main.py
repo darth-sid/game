06ce3442ec6412f65ba937642c59ec4e1729b274
darth-sid <55854690+darth-sid@users.noreply.github.com>
2020-05-01 11:01:18 -0700

    hope dis work

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello")
+import os, sys, random, time, pygame
+from pygame.locals import *
+import pygame.gfxdraw
+
+pygame.init()
+
+size = 320
+screen = pygame.display.set_mode((size, size))
+screen.fill((0,0,0))
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            running = False
